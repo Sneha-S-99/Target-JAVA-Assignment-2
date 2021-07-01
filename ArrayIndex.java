6fa714b61570bcd71c4cc8b8098998c81e6d99e5
Sneha-S-99 <86793624+Sneha-S-99@users.noreply.github.com>
2021-07-01 21:27:06 +0530

    Add files via upload

---
+++ ArrayIndex.java
@@ -0,0 +1,29 @@
+package sneha.target1;
+
+import java.util.Scanner;
+
+public class ArrayIndex {
+	public static void main(String[] args) {
+		int array[]= new int[6];
+		int variable=0;
+		for(int i=0;i<6;i++)
+		{
+		    variable+=i;
+			array[i]=variable;
+		}
+		try {
+			Scanner scanner=new Scanner(System.in);
+			System.out.println("Enter the index to get the element value:");
+			int value=scanner.nextInt();	
+			System.out.println(array[value]);
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("The last index of the array is 5");
+		}
+		finally {
+			System.out.println("Program successful.");
+		}
+	}
+
+}
